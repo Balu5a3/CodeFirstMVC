54c3b0fd6acc291d4848e2d7097ac438cd538860
BALA MURALI KRISHNA PENUMARTHI <Balu5a3@users.noreply.github.com>
2023-12-02 20:17:25 -0500

    Initial commit

---
+++ CodeFirstMVC/Context/LockContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using CodeFirstMVC.Models;
+
+namespace CodeFirstMVC.Context
+{
+    public class LockContext : DbContext
+    {
+        public DbSet<Lock> Locks { get; set; }
+    }
+}
---
+++ CodeFirstMVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using CodeFirstMVC.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeFirstMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        LockContext db = new LockContext();
+        public ActionResult Index()
+        {
+            return View(db.Locks.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
